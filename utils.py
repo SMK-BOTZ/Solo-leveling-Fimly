94a037a9dbd7467768e8dc85e3d5925c6f415045
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2025-01-01 01:19:37 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,92 @@
+import logging, asyncio, os, re, random, pytz, aiohttp, requests, string, json, http.client
+from datetime import date, datetime
+from config import SHORTLINK_API, SHORTLINK_URL
+from shortzy import Shortzy
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+TOKENS = {}
+VERIFIED = {}
+
+async def get_verify_shorted_link(link):
+    if SHORTLINK_URL == "api.shareus.io":
+        url = f'https://{SHORTLINK_URL}/easy_api'
+        params = {
+            "key": SHORTLINK_API,
+            "link": link,
+        }
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                    data = await response.text()
+                    return data
+        except Exception as e:
+            logger.error(e)
+            return link
+    else:
+  #      response = requests.get(f"https://{SHORTLINK_URL}/api?api={SHORTLINK_API}&url={link}")
+ #       data = response.json()
+  #      if data["status"] == "success" or rget.status_code == 200:
+   #         return data["shortenedUrl"]
+        shortzy = Shortzy(api_key=SHORTLINK_API, base_site=SHORTLINK_URL)
+        link = await shortzy.convert(link)
+        return link
+
+async def check_token(bot, userid, token):
+    user = await bot.get_users(userid)
+    if user.id in TOKENS.keys():
+        TKN = TOKENS[user.id]
+        if token in TKN.keys():
+            is_used = TKN[token]
+            if is_used == True:
+                return False
+            else:
+                return True
+    else:
+        return False
+
+async def get_token(bot, userid, link):
+    user = await bot.get_users(userid)
+    token = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
+    TOKENS[user.id] = {token: False}
+    link = f"{link}verify-{user.id}-{token}"
+    shortened_verify_url = await get_verify_shorted_link(link)
+    return str(shortened_verify_url)
+
+async def is_owner(bot, user_id):
+    """
+    Check if the given user is the owner of the bot.
+
+    :param bot: The bot client instance
+    :param user_id: The user ID to check
+    :return: True if the user is the owner, False otherwise
+    """
+    bot_id = bot.me.id
+    owner = mongo_db.bots.find_one({'bot_id': bot_id})
+    if not owner:
+        return False  # If owner is not found, return False
+    owner_id = int(owner['user_id'])
+    return user_id == owner_id
+
+
+async def verify_user(bot, userid, token):
+    user = await bot.get_users(userid)
+    TOKENS[user.id] = {token: True}
+    tz = pytz.timezone('Asia/Kolkata')
+    today = date.today()
+    VERIFIED[user.id] = str(today)
+
+async def check_verification(bot, userid):
+    user = await bot.get_users(userid)
+    tz = pytz.timezone('Asia/Kolkata')
+    today = date.today()
+    if user.id in VERIFIED.keys():
+        EXP = VERIFIED[user.id]
+        years, month, day = EXP.split('-')
+        comp = date(int(years), int(month), int(day))
+        if comp<today:
+            return False
+        else:
+            return True
+    else:
+        return False
